26bb26c9cc20f915fa925355bec2849b63f2fbfb
Анатолий <handoginanatolij@gmail.com>
2024-11-01 22:24:51 +0600

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (7, "house", 110.5, True)
+print(immutable_var)
+# immutable_var [0] = 15
+# print(immutable_var)
+# Значения элементов кортежа нельзя изменить, потому что кортеж это неизменяемый тип данных.
+mutable_list = ["money", "gold", "daimond", 2]
+mutable_list [1] = "silver"
+mutable_list [3] = 14
+print(mutable_list)
