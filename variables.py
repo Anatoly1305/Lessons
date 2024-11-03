b1f1a4a706995bc0c1e3003e3b1603cd1808d117
Анатолий <handoginanatolij@gmail.com>
2024-11-03 19:33:17 +0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,4 +2,4 @@ total_tasks = 12
 hours_spent = 1.5
 course = 'Python'
 average_time = hours_spent / total_tasks
-print(course, total_tasks, hours_spent, average_time)
+print("Курс:",course,"Всего задач:",total_tasks,"Затрачено часов:",hours_spent,"Среднее время выполнения:",average_time)
